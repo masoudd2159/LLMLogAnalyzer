f412dd707ca5f0161b605303facb7f4e3c51348c
Masoud <masoudd2159@gmail.com>
2026-06-04 10:16:48 +0330

    first commit

---
+++ src/main/java/masoud/dabbaghi/llmloganalyzer/LlmLogAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package masoud.dabbaghi.llmloganalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmLogAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LlmLogAnalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/masoud/dabbaghi/llmloganalyzer/LlmLogAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package masoud.dabbaghi.llmloganalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmLogAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
